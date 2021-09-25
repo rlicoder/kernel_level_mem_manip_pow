ab2a90ab6c3a013b5210d82e48bc626b93924f46
rlicoder <rl.nguyen@outlook.com>
2021-09-24 23:36:08 -0700

    removed debugging code

--- host-client/Game.cpp
+++ host-client/Game.cpp
@@ -169,8 +169,6 @@ float CalculateFov(Entity& from, Entity& target)
 
 QAngle CalculateBestBoneAim(Entity& from, uintptr_t t, float max_fov, float smooth, int bone)
 {
-    std::cout << smooth << " " << bone << std::endl;
-    std::cout << "before zoom: " << max_fov << std::endl;
 	Entity target = getEntity(t);
 	if(firing_range)
 	{
@@ -189,7 +187,6 @@ QAngle CalculateBestBoneAim(Entity& from, uintptr_t t, float max_fov, float smoo
 	
 	Vector LocalCamera = from.GetCamPos();
 	Vector TargetBonePosition = target.getBonePosition(bone);
-    //std::cout << "TBP: " << TargetBonePosition.x << " " << TargetBonePosition.y << std::endl;
 	QAngle CalculatedAngles = QAngle(0, 0, 0);
 	
 	WeaponXEntity curweap = WeaponXEntity();
@@ -202,7 +199,6 @@ QAngle CalculateBestBoneAim(Entity& from, uintptr_t t, float max_fov, float smoo
 	{
 		max_fov *= zoom_fov/90.0f;
 	}
-    std::cout << "after: " << max_fov << std::endl;
 
 	/*
 	//simple aim prediction
@@ -235,8 +231,6 @@ QAngle CalculateBestBoneAim(Entity& from, uintptr_t t, float max_fov, float smoo
     	CalculatedAngles = Math::CalcAngle(LocalCamera, TargetBonePosition);
 	QAngle ViewAngles = from.GetViewAngles();
 	QAngle SwayAngles = from.GetSwayAngles();
-    //std::cout << "View: " << ViewAngles.x << " " << ViewAngles.y << std::endl;
-    //std::cout << "Sway: " << SwayAngles.x << " " << SwayAngles.y << std::endl;
 	//remove sway and recoil
 	if(aim_no_recoil)
 		CalculatedAngles-=SwayAngles-ViewAngles;
