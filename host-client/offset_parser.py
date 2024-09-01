54b8a25eb7c79883728911f4bfd94cf31f08c78d
Sebastian-Hall <sebastianhall13@gmail.com>
2024-09-01 11:06:02 -0700

    Add some really really terrible error handling to offset parser
    - Also, update offsets

--- host-client/offset_parser.py
+++ host-client/offset_parser.py
@@ -2,6 +2,7 @@ import logging
 import sys
 from offset_name_map import names
 
+
 if(len(sys.argv) != 2):
     exit('Usage: python3 offset_parser.py <input_file_name>')
 
@@ -34,12 +35,34 @@ for key in names.keys():
         offsets_file.write(f'#define {names[key]}\t\t{offsets[key]}\n')
     else:
         logging.warning(f'{key} was not found in offsets paste')
-offsets_file.write(f'#define OFFSET_BONES\t\t{offsets["m_nForceBone"]} + 0x48\n')
-offsets_file.write(f'#define OFFSET_VIEWANGLES\t\t{offsets["m_ammoPoolCapacity"]} - 0x14\n')
-offsets_file.write(f'#define OFFSET_BREATH_ANGLES\t\tOFFSET_VIEWANGLES - 0x10\n')
-offsets_file.write(f'#define OFFSET_ZOOM_FOV\t\tOFFSET_PLAYER_DATA + OFFSET_CURRENT_ZOOM\n')
+try:
+    offsets_file.write(f'#define OFFSET_BONES\t\t{offsets["m_nForceBone"]} + 0x48\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
+try:
+    offsets_file.write(f'#define OFFSET_VIEWANGLES\t\t{offsets["m_ammoPoolCapacity"]} - 0x14\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
+try:
+    offsets_file.write(f'#define OFFSET_BREATH_ANGLES\t\tOFFSET_VIEWANGLES - 0x10\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
+try:
+    offsets_file.write(f'#define OFFSET_ZOOM_FOV\t\tOFFSET_PLAYER_DATA + OFFSET_CURRENT_ZOOM\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
 # offsets_file.write(f'#define OFFSET_VISIBLE_TIME\t\t0x1aa0\n')
-offsets_file.write(f'#define OFFSET_LOCAL_ENT\t\t{offsets["LocalPlayer"]}\n')
+try:
+    offsets_file.write(f'#define OFFSET_LOCAL_ENT\t\t{offsets["LocalPlayer"]}\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
 # offsets_file.write(f'#define OFFSET_BULLET_SPEED\t\tOFFSET_VISIBLE_TIME + 0x4cc\n')
 # offsets_file.write(f'#define OFFSET_BULLET_SCALE\t\tOFFSET_VISIBLE_TIME + 0x4d4\n')
-offsets_file.write(f'#endif\n')
+try:
+    offsets_file.write(f'#endif\n')
+except KeyError as err:
+    logging.warning(f'KEY NOT FOUND IN MAP: {err}')
+
+
+
+logging.info('FINISHED')
